f0292347e572cbe5d80df0846d1fbad91cd4f808
rclt1-ua <rclt1@alu.ua.es>
2024-02-23 12:47:56 +0100

    Modulo delete

--- p1/prac1.cc
+++ p1/prac1.cc
@@ -140,9 +140,9 @@ int main(){
 void addAcademicYear(vector<AcademicYear> &listYears){
     int largo = listYears.size(), // LARGO DE LA LISTA DE ANYOS
         encontrado = -1, // VARIABLE PARA SABER SI EL ANYO YA EXISTE
-        recorredor; // RECORREDOR PARA EL FOR
-    string idY; // VARIABLE PARA AÑADIR EL ANYO
-    int idYInt; // VARIABLE PARA AÑADIR EL ANYO EN ENTERO
+        recorredor, // RECORREDOR PARA EL FOR
+        idYInt; // VARIABLE PARA LEER EL ANYO EN ENTERO
+    string idY; // VARIABLE PARA LEER EL ANYO
     AcademicYear anyo; // VARIABLE PARA AÑADIR EL ANYO
     
 
@@ -185,9 +185,9 @@ void addAcademicYear(vector<AcademicYear> &listYears){
 void addAcademicYear(vector<AcademicYear> *listYears){
     int largo = listYears->size(), // LARGO DE LA LISTA DE ANYOS
         encontrado = -1, // VARIABLE PARA SABER SI EL ANYO YA EXISTE
-        recorredor; // IDY PARA EL ID ANYO Y RECORREDOR PARA EL FOR
-    string idY;
-    int idYInt;
+        recorredor, // RECORREDOR PARA EL FOR
+        idYInt; // VARIABLE PARA LEER EL ANYO EN ENTERO
+    string idY; // VARIABLE PARA LEER EL ANYO
     AcademicYear anyo; // VARIABLE PARA AÑADIR EL ANYO
     
 
@@ -223,4 +223,51 @@ void addAcademicYear(vector<AcademicYear> *listYears){
             listYears->push_back(anyo);
         }
     }
+}
+
+
+
+//Funcion para eliminar un anyo academico
+void deleteAcademicYear(vector<AcademicYear> &listYears){
+    int largo = listYears.size(), // LARGO DE LA LISTA DE ANYOS
+        eliminado = -1, // VARIABLE PARA SABER SI EL ANYO YA EXISTE
+        seIntrodujo = 1, // VARIABLE PARA SABER SI EL ANYO NO EXISTE
+        recorredor, // IDY PARA EL ID ANYO Y RECORREDOR PARA EL FOR
+        idYInt;
+    string idY;
+
+
+    do{ // SE HACE UN DO WHILE PARA QUE SE VUELVA A PEDIR EL ID DEL ANYO SI NO SE ENCUENTRA
+        cout<< "Enter academic year: " << endl;
+        cin >> idY;
+
+        if(largo == 0){ // SI NO SE EXISTE NINGUN ANYO, SE MUESTRA UN ERROR
+            error(ERR_NOT_EXIST);
+        }
+
+        if(idY.empty()){ // SI NO SE INTRODUCE NINGUN ANYO, SE MUESTRA UN ERROR, EN ESTE CASO SE SALE DEL BUCLE DEVOLVIENDO EL ERROR
+            error(ERR_EMPTY);
+            seIntrodujo = -1;
+        }    
+        else{ // SI SE ENCUENTRA ANYOS, SE BUSCA EL ANYO Y SE ELIMINA
+            seIntrodujo = 1;
+
+            idYInt = stoi(idY);
+
+            for(int recorredor = 0; recorredor < largo && eliminado == -1; recorredor++){
+                if(listYears[recorredor].id == idYInt){ // SE VERIFICA EL ANYO SEGUN EL ID
+                    listYears.erase(listYears.begin() + recorredor); // SE ELIMINA EL ANYO
+
+                    eliminado = 1; // SE CAMBIA EL VALOR DE LA VARIABLE PARA SABER QUE YA SE ELIMINO
+                }
+            }
+            
+            if(eliminado == -1){ // SI NO SE ENCUENTRA EL ANYO, SE MUESTRA UN ERROR, NO ES LO MISMO QUE EL ERROR DE ARRIBA,
+                                // PORQUE AQUI SE BUSCA EL ANYO Y NO SE ENCUENTRA, 
+                                // MIENTRAS QUE EN EL ERROR DE ARRIBA, NO SE BUSCA PORQUE NO HAY DATOS
+                error(ERR_NOT_EXIST);
+            }
+        }
+    } 
+    while(eliminado == -1 && seIntrodujo == 1);
 }
